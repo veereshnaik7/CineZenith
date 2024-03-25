216e270051d1f619564a2624f6142b1d8b6f340c
veeereshnaik7 <jerryveeru1@gmail.com>
2024-03-25 06:04:18 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import About from './components/Navbar'
+import Movies from './components/Movies'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/about' element={<About/>}/>
+     <Route path='/movies' element={<Movies/>}/>
+     
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
