e6077284ddd4badc4549808104f870af55f6e8aa
veeereshnaik7 <jerryveeru1@gmail.com>
2024-03-26 11:55:22 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,6 @@ import { failure } from "../reduxToolkit/cineZenithSlice";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [trailerkey, settrailerkey] = useState("");
@@ -24,7 +23,6 @@ const Home = () => {
   const upComingMovies = useSelector(
     (state) => state.cineZenithSlice.upComingMovies
   );
-
 
   const watchListMovies = useSelector(
     (state) => state.cineZenithSlice.watchListMovies
@@ -93,7 +91,7 @@ const Home = () => {
         </h4>
       </div>
 
-      <h4 className="trail">Latest Trailers</h4>
+      <h3>Latest Trailers</h3>
       <div className="outer">
         {loading && (
           <ColorRing
@@ -122,11 +120,7 @@ const Home = () => {
               original_language,
               adult,
             }) => (
-              <div
-                className="trailer-card"
-                key={id}
-                onMouseOver={() => handleTrailerdetails({ id })}
-              >
+              <div className="t-card" key={id} onClick={() => handleTrailerdetails({ id })} onMouseOver={() => handleTrailerdetails({ id })}>
                 <img
                   src={`https://image.tmdb.org/t/p/original/${poster_path}`}
                   alt=""
@@ -135,8 +129,10 @@ const Home = () => {
                   {title} &nbsp; &nbsp;
                   {release_date}
                 </p>
-
-                <button className="card-btn">
+                <button
+                  
+                  className="card-btn"
+                >
                   <a
                     target="_blank"
                     href={`https://www.youtube.com/watch?v=${trailerkey}`}
@@ -148,7 +144,7 @@ const Home = () => {
             )
           )}
         <div className="logout" title="logout">
-        <button class="authbtn" onClick={handleLogout}>
+          <button class="authbtn" onClick={handleLogout}>
             <div class="sign">
               <svg viewBox="0 0 512 512">
                 <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
