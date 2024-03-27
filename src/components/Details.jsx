050b91a52dc38b7ca3b887b790b9745f0029c01e
veeereshnaik7 <jerryveeru1@gmail.com>
2024-03-27 12:45:45 +0530

    changes

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -66,14 +66,14 @@ const Details = () => {
 
   useEffect(() => {
     handleCastMovies();
-    handleTrailerdetails();
+    
   }, []);
   const backgroundimg = {
     backgroundImage: `url(https://image.tmdb.org/t/p/original/${backdrop_path})`,
   };
   return (
     <div className="main-div" id="main-div">
-      <div className="details" style={backgroundimg}>
+      <div className="details" style={backgroundimg} onClick={()=>handleTrailerdetails()}>
         <div className="details-img">
           <img
             className="poster-path-img"
